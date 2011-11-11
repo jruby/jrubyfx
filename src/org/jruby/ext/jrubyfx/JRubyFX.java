8f630333573d06462253e80754cd047ced7ada8b
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-11-11 21:46:35 +0900

    Refactoring

--- src/org/jruby/ext/jrubyfx/JRubyFX.java
+++ src/org/jruby/ext/jrubyfx/JRubyFX.java
@@ -2,7 +2,6 @@ package org.jruby.ext.jrubyfx;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.jruby.Ruby;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.javasupport.Java;
 import org.jruby.javasupport.JavaObject;
@@ -12,34 +11,38 @@ public class JRubyFX extends Application {
     public static IRubyObject handler;
 
     public static void start(IRubyObject obj) {
+        if (obj == null) {
+            throw new NullPointerException("Application required");
+        }
         handler = obj;
         Application.launch((String[])null);
     }
 
     @Override
     public void init() throws Exception {
-        if (handler.respondsTo("init")) {
-            handler.callMethod(ctx(), "init");
-        }
+        callMethodIfPossible("init");
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Ruby runtime = Ruby.getGlobalRuntime();
         IRubyObject[] args = new IRubyObject[] {
-            Java.wrap(runtime, JavaObject.wrap(runtime, primaryStage))
+            Java.wrap(ctx().runtime, JavaObject.wrap(ctx().runtime, primaryStage))
         };
         handler.callMethod(ctx(), "start", args);
     }
 
     @Override
     public void stop() throws Exception {
-        if (handler.respondsTo("stop")) {
-            handler.callMethod(ctx(), "stop");
-        }
+        callMethodIfPossible("stop");
     }
 
     private ThreadContext ctx() {
-        return Ruby.getGlobalRuntime().getCurrentContext();
+        return handler.getRuntime().getCurrentContext();
+    }
+
+    private void callMethodIfPossible(String methodName) {
+        if (handler.respondsTo(methodName)) {
+            handler.callMethod(ctx(), methodName);
+        }
     }
 }
