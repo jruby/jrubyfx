09c7c60d2f08555f6fb4b1a5e82639c4f9f4d237
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-11-14 00:32:13 +0900

    WebView sample cleanups

--- sample/web_view.rb
+++ sample/web_view.rb
@@ -2,6 +2,7 @@ require 'java'
 require 'jfxrt'
 require 'jrubyfx'
 
+java_import 'javafx.beans.value.ChangeListener'
 java_import 'javafx.scene.Group'
 java_import 'javafx.scene.Scene'
 java_import 'javafx.scene.control.TextField'
@@ -18,39 +19,64 @@ java_import 'javafx.geometry.VPos'
 java_import 'org.jruby.ext.jrubyfx.JRubyFX'
 
 class WebViewApp
-  MAX = java.lang.Double::MAX_VALUE
+  DEFAULT_URL = "http://jruby.org"
 
   def start(stage)
-    url = "http://jruby.org"
-    root = Group.new
-    view = WebView.new
-    view.set_min_size(500, 400)
-    view.set_pref_size(500, 400)
-    view.engine.load(url)
-    location = TextField.new(url)
-    go = Button.new("Go")
+    # nodes
+    view = create(WebView) {
+      set_min_size(500, 400)
+      set_pref_size(500, 400)
+      engine.load(DEFAULT_URL)
+    }
+    location = create(TextField, DEFAULT_URL)
+    go = create(Button, 'Go')
     go.default_button = true
-    action = proc { |event|
+    # actions
+    location.on_action = go.on_action = proc { |event|
       view.engine.load(location.text)
     }
-    go.on_action = location.on_action = action
-    view.engine.location_property.add_listener(proc { |observable, old_value, new_value|
-      location.text = new_value
-    })
-    grid = GridPane.new
-    grid.vgap = 5
-    grid.hgap = 5
-    GridPane.set_constraints(location, 0, 0, 1, 1, HPos::CENTER, VPos::CENTER, Priority::ALWAYS, Priority::SOMETIMES)
-    GridPane.set_constraints(go,       1, 0)
-    GridPane.set_constraints(view,     0, 1, 2, 1, HPos::CENTER, VPos::CENTER, Priority::ALWAYS, Priority::ALWAYS)
-    grid.column_constraints <<
-      ColumnConstraints.new(100, 100, MAX, Priority::ALWAYS, HPos::CENTER, true) <<
-      ColumnConstraints.new( 40,  40,  40, Priority::NEVER,  HPos::CENTER, true)
-    grid.children << location << go << view
-    root.children << grid
-    stage.scene = Scene.new(root)
+    view.engine.location_property.add_listener(
+      listener(ChangeListener, :changed) { |observable, old_value, new_value|
+        location.text = new_value
+      }
+    )
+    # layout
+    grid = create(GridPane) {
+      vgap = 3
+      hgap = 2
+      GridPane.set_constraints(location, 0, 0, 1, 1)
+      GridPane.set_constraints(go,       1, 0)
+      GridPane.set_constraints(view,     0, 1, 1, 1, HPos::LEFT, VPos::CENTER)
+      column_constraints <<
+        ColumnConstraints.new(100, 460, 500, Priority::ALWAYS, HPos::CENTER, true) <<
+        ColumnConstraints.new( 40,  40,  40, Priority::NEVER,  HPos::CENTER, true)
+      children << location << go << view
+    }
+    #
+    stage.scene = Scene.new(create(Group) { children << grid })
     stage.show
   end
+
+private
+
+  def create(klass, *args, &block)
+    obj = klass.new(*args)
+    obj.instance_eval(&block) if block_given?
+    obj
+  end
+
+  def listener(mod, name, &block)
+    obj = Class.new { include mod }.new
+    obj.instance_eval do
+      @name = name
+      @block = block
+      def method_missing(msg, *a, &b)
+        @block.call(*a, &b) if msg == @name
+      end
+    end
+    obj
+  end
+
 end
 
 JRubyFX.start(WebViewApp.new)
