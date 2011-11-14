8a97901a4337ec16d3e6b9882fbd07a5fc0d1139
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-11-14 17:40:06 +0900

    Simplified hello_jrubyfx.rb sample with utils.rb

--- sample/hello_jrubyfx.rb
+++ sample/hello_jrubyfx.rb
@@ -1,9 +1,7 @@
 # Original version is here: http://www.oracle.com/technetwork/jp/ondemand/java/20110519-java-a-2-sato-400530-ja.pdf
 # Modified by Hiroshi Nakamura <nahi@ruby-lang.org>
 
-require 'java'
-require 'jfxrt'
-require 'jrubyfx'
+require_relative('utils')
 
 java_import 'javafx.animation.KeyFrame'
 java_import 'javafx.animation.KeyValue'
@@ -15,39 +13,24 @@ java_import 'javafx.scene.shape.Rectangle'
 java_import 'javafx.stage.Stage'
 java_import 'javafx.util.Duration'
 
-# TODO: temporary manual bootstrap
-java_import 'org.jruby.ext.jrubyfx.JRubyFX'
-
 class MyApp
-  def init
-  end
+  include Utils
 
   def start(stage)
-    stage.title = 'Hello JRubyFX'
-    stage.width = 300
-    stage.height = 200
-    group = Group.new
-    scene = Scene.new(group)
-    scene.fill = Color::DARKBLUE
-    stage.scene = scene
-    rect = Rectangle.new
-    rect.x = 10
-    rect.y = 40
-    rect.width = 50
-    rect.height = 50
-    rect.fill = Color::RED
-    group.children << rect
-    timeline = Timeline.new
-    timeline.cycle_count = Timeline::INDEFINITE
-    timeline.auto_reverse = true
-    kf1 = KeyFrame.new(Duration::ZERO, KeyValue.new(rect.translateXProperty, 0))
-    kf2 = KeyFrame.new(Duration.new(1000), KeyValue.new(rect.translateXProperty, 200))
-    timeline.key_frames << kf1<< kf2
-    timeline.play
-    stage.show
-  end
-
-  def stop
+    root = build(Group) {
+      children <<
+        build(Rectangle, x: 10, y: 40, width: 50, height: 50, fill: Color::RED) {
+          kf1 = KeyFrame.new(Duration::ZERO, KeyValue.new(translateXProperty, 0))
+          kf2 = KeyFrame.new(Duration.millis(1000), KeyValue.new(translateXProperty, 200))
+          build(Timeline, cycle_count: Timeline::INDEFINITE, auto_reverse: true) {
+            key_frames << kf1 << kf2
+          }.play
+        }
+    }
+    with(stage,
+         width: 300, height: 200,
+         title: 'Hello JRubyFX',
+         scene: build(Scene, root, fill: Color::DARKBLUE)).show
   end
 end
 
