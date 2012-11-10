689a37004590ceeda7509d42bb0c6c8148903767
Thomas E. Enebo <tom.enebo@gmail.com>
2012-11-10 11:14:04 -0600

    add_change_listener for all observable values to not require jrubyfx listener method to be called directly.  New updated binding_app sample using new syntax

---
+++ lib/jrubyfx/core_ext/observable_value.rb
@@ -0,0 +1,17 @@
+module Java::javafx::beans::value::ObservableValue
+  include JRubyFX # FIXME: Listener support should be own module
+
+  java_import Java::javafx.beans.value.ChangeListener
+
+
+  def add_change_listener(&block)
+    add_listener(listener(ChangeListener, :changed, &block))
+# Should work but gets access issues
+#    java_send :addListener, [ChangeListener.java_class], block
+  end
+
+  # FIXME: Not sure how to remove with this API.  We are passing in a proc
+  # and we would need to examine each proc to determine which listener to
+  # remove.  Probably a way to do it in each derived real class which actually
+  # stores the listeners.
+end
