3cbd011c2075959084530d66cd0280b7055e42da
Patrick Plenefisch <simonpatp@gmail.com>
2012-12-11 23:43:30 -0500

    FXML onAction 'compilies' now and can show

--- samples/AnalogClock.rb
+++ samples/AnalogClock.rb
@@ -15,7 +15,7 @@ class AnalogClock
       s.title, s.width, s.height = 'This is RUBY!!!!', 600,600
       #s.resizable = false
       #group = Group.new.tap {|g| g.children << create_content }
-      s.scene = Scene.new(load_fxml "#{File.dirname(__FILE__)}/Sample.fxml", TestController.new(nil, nil))
+      s.scene = Scene.new(load_fxml "#{File.dirname(__FILE__)}/Sample.fxml", TestController.newInstance)
       #.tap do |sc|
       #  sc.fill = nil # Completes transparency
         #sc.set_on_key_pressed { |e| java.lang.System.exit(0) }
@@ -26,26 +26,28 @@ class AnalogClock
 
 end
 
-class TestController
+class TestControllerj
   include Java.javafx.fxml.Initializable
   #TODO: SOOPER HACK!!!! BAD BAD BAD
   #@java_aliases = {} if @java_aliases == nil
   #java_alias :initialize, :initialize
+  add_method_signature :click, [Java.javafx.event.ActionEvent]
   def initialize(fxmlFileLocation, resources)
     puts "initalized"
     p fxmlFileLocation
     p resources
   end
   
-#  def click(stuff)
-#    p "Clicked"
-#    p stuff
-#  end
+  def click(stuff)
+    p "Clicked"
+    p stuff
+  end
 end
- #TestController.add_method_signature("click", [Java.javafx.event.ActionEvent])
- #TestController.add_method_signature("initialize", [Java.java.net.URL, Java.java.util.ResourceBundle])
+# TestController."click")
+ TestControllerj.add_method_signature("initialize", [java.lang.Void::TYPE, Java.java.net.URL, Java.java.util.ResourceBundle])
     
-#TestController.become_java!
+TestController = TestControllerj.become_java!
+p TestController.declared_methods
  #   puts "AnalogClock.rb:raw start()@" + Time.now.usec.to_s
 AnalogClock.start
  #   puts "AnalogClock.rb:raw start()-ed@" + Time.now.usec.to_s
