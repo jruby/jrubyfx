ad25d2172fad2ee01159660d92e50e688f20d430
Patrick Plenefisch <simonpatp@gmail.com>
2013-01-03 12:28:02 -0500

    Nesting modules to prevent rake failing

--- lib/jrubyfx/jfx_imports.rb
+++ lib/jrubyfx/jfx_imports.rb
@@ -30,72 +30,70 @@ rescue
   fail "JavaFX runtime not found.  Please install Java 7u4 or newer or set environment variable JAVAFX_DIR to the folder that contains jfxrt.jar"
 end
 
-module JRubyFX
-  
-end
-
 # If you need JavaFX, just include this module. Its sole purpose in life is to
 # import all JavaFX stuff, plus a few useful Java classes (like Void)
-module JRubyFX::FXImports
+module JRubyFX
+  module FXImports
   
-  # If something is missing, just java_import it in your code.
-  # And then ask us to put it in this list
+    # If something is missing, just java_import it in your code.
+    # And then ask us to put it in this list
   
-  ## 
-  # This is the list of all classes in JavaFX that most apps should care about.
-  # It is a hashmaps with the leafs as arrays. Where a leaf also contains more 
-  # packages, the hashmap key is "" (empty string). You can utilize this constant
-  # to save yourself some typing when adding code for most/all of the JavaFX
-  # classes by using either `Hash.flat_tree_inject` from jrubyfx/utils.rb or
-  # writing your own traversal function
-  #
-  JFX_CLASS_HIERARCHY = { :javafx => {
-      :animation => %w[Animation AnimationTimer FadeTransition FillTransition Interpolator KeyFrame KeyValue ParallelTransition PathTransition 
+    ## 
+    # This is the list of all classes in JavaFX that most apps should care about.
+    # It is a hashmaps with the leafs as arrays. Where a leaf also contains more 
+    # packages, the hashmap key is "" (empty string). You can utilize this constant
+    # to save yourself some typing when adding code for most/all of the JavaFX
+    # classes by using either `Hash.flat_tree_inject` from jrubyfx/utils.rb or
+    # writing your own traversal function
+    #
+    JFX_CLASS_HIERARCHY = { :javafx => {
+        :animation => %w[Animation AnimationTimer FadeTransition FillTransition Interpolator KeyFrame KeyValue ParallelTransition PathTransition 
         PauseTransition RotateTransition ScaleTransition SequentialTransition StrokeTransition Timeline Transition TranslateTransition],
-      :application => ['Platform'],
-      :beans => {
-        :property => ['SimpleDoubleProperty'],
-        #TODO: import more
-        :value => ['ChangeListener']  
-      },
-      :collections => ['FXCollections'],
-      :event => %w[Event ActionEvent EventHandler],
-      :fxml => ['Initializable', 'LoadException'],
-      :geometry => %w[HorizontalDirection HPos Insets Orientation Pos Side VerticalDirection VPos], 
-      :scene => {
-        '' => %w[Group Node Parent Scene],
-        :canvas => ['Canvas'],
-        :chart => %w[Axis CategoryAxis Chart LineChart NumberAxis XYChart],
-        # TODO: import more of these
-        :control => %w[Accordion Button Cell CheckBox CheckBoxTreeItem CheckMenuItem ChoiceBox ColorPicker ComboBox ContextMenu Hyperlink 
+        :application => ['Platform'],
+        :beans => {
+          :property => ['SimpleDoubleProperty'],
+          #TODO: import more
+          :value => ['ChangeListener']  
+        },
+        :collections => ['FXCollections'],
+        :event => %w[Event ActionEvent EventHandler],
+        :fxml => ['Initializable', 'LoadException'],
+        :geometry => %w[HorizontalDirection HPos Insets Orientation Pos Side VerticalDirection VPos], 
+        :scene => {
+          '' => %w[Group Node Parent Scene],
+          :canvas => ['Canvas'],
+          :chart => %w[Axis CategoryAxis Chart LineChart NumberAxis XYChart],
+          # TODO: import more of these
+          :control => %w[Accordion Button Cell CheckBox CheckBoxTreeItem CheckMenuItem ChoiceBox ColorPicker ComboBox ContextMenu Hyperlink 
           Label ListCell ListView Menu MenuBar MenuButton MenuItem Pagination PasswordField PopupControl ProgressBar ProgressIndicator RadioButton 
           RadioMenuItem ScrollBar ScrollPane Separator SeparatorMenuItem Slider SplitMenuButton SplitPane Tab TableView TableColumn TabPane TextArea 
           TextField TitledPane ToggleButton ToggleGroup ToolBar Tooltip TreeItem TreeView ContentDisplay OverrunStyle SelectionMode],
-        :effect => %w[Blend BlendMode Bloom BlurType BoxBlur ColorAdjust ColorInput DisplacementMap DropShadow GaussianBlur Glow ImageInput 
+          :effect => %w[Blend BlendMode Bloom BlurType BoxBlur ColorAdjust ColorInput DisplacementMap DropShadow GaussianBlur Glow ImageInput 
           InnerShadow Lighting MotionBlur PerspectiveTransform Reflection SepiaTone Shadow],
-        :image => %w[Image ImageView PixelReader PixelWriter],
-        :input => %w[Clipboard ContextMenuEvent DragEvent GestureEvent InputEvent InputMethodEvent KeyEvent 
+          :image => %w[Image ImageView PixelReader PixelWriter],
+          :input => %w[Clipboard ContextMenuEvent DragEvent GestureEvent InputEvent InputMethodEvent KeyEvent 
           Mnemonic MouseDragEvent MouseEvent RotateEvent ScrollEvent SwipeEvent TouchEvent ZoomEvent],
-        :layout => %w[AnchorPane BorderPane ColumnConstraints FlowPane GridPane HBox Pane Priority RowConstraints StackPane TilePane VBox],
-        :media => %w[Media MediaPlayer MediaView],
-        # TODO: fill this out
-        :paint => %w[Color CycleMethod ImagePattern LinearGradient Paint RadialGradient Stop],
-        :shape => %w[Arc ArcTo ArcType Circle ClosePath CubicCurve CubicCurveTo Ellipse FillRule HLineTo Line LineTo MoveTo Path PathElement 
+          :layout => %w[AnchorPane BorderPane ColumnConstraints FlowPane GridPane HBox Pane Priority RowConstraints StackPane TilePane VBox],
+          :media => %w[Media MediaPlayer MediaView],
+          # TODO: fill this out
+          :paint => %w[Color CycleMethod ImagePattern LinearGradient Paint RadialGradient Stop],
+          :shape => %w[Arc ArcTo ArcType Circle ClosePath CubicCurve CubicCurveTo Ellipse FillRule HLineTo Line LineTo MoveTo Path PathElement 
           Polygon Polyline QuadCurve QuadCurveTo Rectangle Shape StrokeLineCap StrokeLineJoin StrokeType SVGPath VLineTo],
-        :text => %w[Font FontPosture FontSmoothingType FontWeight Text TextAlignment TextBoundsType],
-        :transform => %w[Affine Rotate Scale Shear Translate],
-        :web => ['WebView', 'HTMLEditor']
-      },
-      :stage => %w[DirectoryChooser FileChooser Modality Popup PopupWindow Screen Stage StageStyle Window WindowEvent],
-      :util => ['Duration']
+          :text => %w[Font FontPosture FontSmoothingType FontWeight Text TextAlignment TextBoundsType],
+          :transform => %w[Affine Rotate Scale Shear Translate],
+          :web => ['WebView', 'HTMLEditor']
+        },
+        :stage => %w[DirectoryChooser FileChooser Modality Popup PopupWindow Screen Stage StageStyle Window WindowEvent],
+        :util => ['Duration']
+      }
     }
-  }
   
-  # Imports all the listed JavaFX classes
-  java_import *(JFX_CLASS_HIERARCHY.flat_tree_inject do |res, name, values|
-      name = "#{name.to_s}."
-      name = "" if name == "."
-      res.concat(values.map{|i| "#{name}#{i}"})
-    end)
-  java_import 'java.lang.Void'
+    # Imports all the listed JavaFX classes
+    java_import *(JFX_CLASS_HIERARCHY.flat_tree_inject do |res, name, values|
+        name = "#{name.to_s}."
+        name = "" if name == "."
+        res.concat(values.map{|i| "#{name}#{i}"})
+      end)
+    java_import 'java.lang.Void'
+  end
 end
--- lib/jrubyfx_tasks.rb
+++ lib/jrubyfx_tasks.rb
@@ -22,86 +22,88 @@ require 'rake'
 require 'tmpdir'
 
 # This module contains utilities to jarify an app, and can be used in a rakefile or a running app.
-module JRubyFX::Tasks
-  extend Rake::DSL
-  # Base URL of JRuby-complete.jar download location
-  BASE_URL='http://repository.codehaus.org/org/jruby/jruby-complete'
-  
-  ##
-  # Downloads the jruby-complete jar file for `jruby_version` and save in 
-  # ~/.jruby-jar/jruby-complete.jar unless it already exits. If the jar is 
-  # corrupt or an older version, set force to true to delete and re-download
-  def download_jruby(jruby_version, force=false)
-    dist = "#{ENV['HOME']}/.jruby-jar"
-    unless force || (File.exists?("#{dist}/jruby-complete.jar") && File.size("#{dist}/jruby-complete.jar") > 0)
-      mkdir_p dist
+module JRubyFX
+  module Tasks
+    extend Rake::DSL
+    # Base URL of JRuby-complete.jar download location
+    BASE_URL='http://repository.codehaus.org/org/jruby/jruby-complete'
+
+    ##
+    # Downloads the jruby-complete jar file for `jruby_version` and save in 
+    # ~/.jruby-jar/jruby-complete.jar unless it already exits. If the jar is 
+    # corrupt or an older version, set force to true to delete and re-download
+    def download_jruby(jruby_version, force=false)
+      dist = "#{ENV['HOME']}/.jruby-jar"
+      unless force || (File.exists?("#{dist}/jruby-complete.jar") && File.size("#{dist}/jruby-complete.jar") > 0)
+        mkdir_p dist
+        base_dir = Dir.pwd
+        cd dist
+        puts "JRuby complete jar not found. Downloading... (May take awhile)"
+        download(jruby_version)
+        cd base_dir
+      end
+    end
+
+    ## 
+    # Creates a full jar from the given source pattern (must be a pattern to match
+    # files), with the given main script as the script to launch when the jarfile
+    # is run. The output jar is saved in the `target` dir, which also doubles as a
+    # temporary work dir. `jar` is the executable that makes jars. If `target` is
+    # nill then a random temporary directory is created, and output_jar is the
+    # full path to the jar file to save 
+    def jarify_jrubyfx(src="src/*" ,main_script=nil, target="target", output_jar="jrubyfx-app.jar", jar="jar")
+      if target_was_nil = target == nil
+        target = Dir.mktmpdir("jrubyfx")
+        final_jar = output_jar
+        output_jar = File.basename output_jar
+      end
+
+      mkdir_p target
+
+      #copy jruby jar file in, along with script and our rb files
+      cp "#{ENV['HOME']}/.jruby-jar/jruby-complete.jar", "#{target}/#{output_jar}"
+
+      #copy source in
+      FileList[src].each do |iv_srv|
+        cp iv_srv, "#{target}/#{File.basename(iv_srv)}" if main_script == nil || main_script != iv_srv
+      end
+      cp main_script, "#{target}/jar-bootstrap.rb" unless main_script == nil
+
+      unless File.exists? "#{target}/jar-bootstrap.rb"
+        puts "@"*79
+        puts "@#{"!!!WARNING!!!".center(79-2)}@"
+        puts "@#{"jar-bootstrap.rb NOT FOUND!".center(79-2)}@"
+        puts "@#{"Did you set main_src= or have jar-bootstrap in src= ?".center(79-2)}@"
+        puts "@"*79
+      end
+
+      #copy our libs in
+      FileList["#{File.dirname(__FILE__)}/*"].each do |librb|
+        cp_r librb, target
+      end
+
+      # edit the jar
       base_dir = Dir.pwd
-      cd dist
-      puts "JRuby complete jar not found. Downloading... (May take awhile)"
-      download(jruby_version)
+      cd target
+      sh "#{jar} ufe '#{output_jar}' org.jruby.JarBootstrapMain *"
+      chmod 0775, output_jar
       cd base_dir
-    end
-  end
 
-  ## 
-  # Creates a full jar from the given source pattern (must be a pattern to match
-  # files), with the given main script as the script to launch when the jarfile
-  # is run. The output jar is saved in the `target` dir, which also doubles as a
-  # temporary work dir. `jar` is the executable that makes jars. If `target` is
-  # nill then a random temporary directory is created, and output_jar is the
-  # full path to the jar file to save 
-  def jarify_jrubyfx(src="src/*" ,main_script=nil, target="target", output_jar="jrubyfx-app.jar", jar="jar")
-    if target_was_nil = target == nil
-      target = Dir.mktmpdir("jrubyfx")
-      final_jar = output_jar
-      output_jar = File.basename output_jar
+      if target_was_nil
+        mv "#{target}/#{output_jar}", final_jar
+        rm_rf target
+      end
     end
-    
-    mkdir_p target
-  
-    #copy jruby jar file in, along with script and our rb files
-    cp "#{ENV['HOME']}/.jruby-jar/jruby-complete.jar", "#{target}/#{output_jar}"
-  
-    #copy source in
-    FileList[src].each do |iv_srv|
-      cp iv_srv, "#{target}/#{File.basename(iv_srv)}" if main_script == nil || main_script != iv_srv
-    end
-    cp main_script, "#{target}/jar-bootstrap.rb" unless main_script == nil
-  
-    unless File.exists? "#{target}/jar-bootstrap.rb"
-      puts "@"*79
-      puts "@#{"!!!WARNING!!!".center(79-2)}@"
-      puts "@#{"jar-bootstrap.rb NOT FOUND!".center(79-2)}@"
-      puts "@#{"Did you set main_src= or have jar-bootstrap in src= ?".center(79-2)}@"
-      puts "@"*79
-    end
-  
-    #copy our libs in
-    FileList["#{File.dirname(__FILE__)}/*"].each do |librb|
-      cp_r librb, target
-    end
-  
-    # edit the jar
-    base_dir = Dir.pwd
-    cd target
-    sh "#{jar} ufe '#{output_jar}' org.jruby.JarBootstrapMain *"
-    chmod 0775, output_jar
-    cd base_dir
-    
-    if target_was_nil
-      mv "#{target}/#{output_jar}", final_jar
-      rm_rf target
-    end
-  end
 
-  private
-  def download(version_string) #:nodoc:
-    File.open("jruby-complete.jar","wb") do |f|
-      f.write(open("#{BASE_URL}/#{version_string}/jruby-complete-#{version_string}.jar").read)
+    private
+    def download(version_string) #:nodoc:
+      File.open("jruby-complete.jar","wb") do |f|
+        f.write(open("#{BASE_URL}/#{version_string}/jruby-complete-#{version_string}.jar").read)
+      end
     end
+
+    module_function :jarify_jrubyfx
+    module_function :download_jruby
+    module_function :download
   end
-  
-  module_function :jarify_jrubyfx
-  module_function :download_jruby
-  module_function :download
 end
