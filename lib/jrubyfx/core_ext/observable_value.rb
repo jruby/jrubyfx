73f39fb88140527b157571cc5c0f7c8bf1961f2e
Patrick Plenefisch <simonpatp@gmail.com>
2013-01-03 22:37:39 -0500

    Using proper way to call correct addListener method for ObservableValues

--- lib/jrubyfx/core_ext/observable_value.rb
+++ lib/jrubyfx/core_ext/observable_value.rb
@@ -2,14 +2,10 @@ require 'jrubyfx'
 
 # JRubyFX DSL extensions for JavaFX ObservableValues
 module Java::javafx::beans::value::ObservableValue
-  include JRubyFX # FIXME: Listener support should be own module
-
   java_import Java::javafx.beans.value.ChangeListener
 
   def add_change_listener(&block)
-    add_listener(JRubyFX.listener(ChangeListener, :changed, &block))
-    # Should work but gets access issues
-    #    java_send :addListener, [ChangeListener.java_class], block
+    java_send :addListener, [ChangeListener.java_class], block
   end
 
   # FIXME: Not sure how to remove with this API.  We are passing in a proc
