39a99e8910d29bfe8e6d6741ade6b6f815773757
Patrick Plenefisch <simonpatp@gmail.com>
2013-01-23 21:36:19 -0500

    Properly caching jfxrt.jar missing error and spewing more helpful information

--- lib/jrubyfx/jfx_imports.rb
+++ lib/jrubyfx/jfx_imports.rb
@@ -25,8 +25,10 @@ begin
     $LOAD_PATH << ENV_JAVA["sun.boot.library.path"].gsub(/[\/\\][amdix345678_]+$/, "") # strip i386 or amd64 (including variants). TODO: ARM
   end
   require 'jfxrt.jar'
-rescue
-  fail "JavaFX runtime not found.  Please install Java 7u4 or newer or set environment variable JAVAFX_DIR to the folder that contains jfxrt.jar"
+rescue  LoadError
+  puts "JavaFX runtime not found.  Please install Java 7u6 or newer or set environment variable JFX_DIR to the folder that contains jfxrt.jar "
+  puts "If you have Java 7u6 or later, this is a bug. Please report to the issue tracker on github. Include your OS version, 32/64bit, and architecture (x86, ARM, PPC, etc)"
+  exit -1
 end
 
 module JRubyFX
