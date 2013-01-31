27fe831e9a4eea237c90806dcd702cef21d0764d
Thomas E. Enebo <tom.enebo@gmail.com>
2013-01-31 15:18:39 -0600

    A fairly complex example of tree view with drag and drop support

--- samples/javafx/tree_view.rb
+++ samples/javafx/tree_view.rb
@@ -1,13 +1,142 @@
+#!/usr/bin/env jruby
+
 require 'jrubyfx'
+
+##
+# Example showing a tree_view and also how complicated you can tailor
+# behavior of a tree_view.  In this example we allow you to drag and drop
+# tree_items around.  There are three things you can see in d&d here:
+# 1. drag onto other menu item will put it as a child in that menu item
+# 2. drag and drop to rearrange items
+# 3. drag and drop into another window...it should paste the contents there
+#
+class DraggableTreeCell < Java::javafx::scene::control::TreeCell
+  include JRubyFX::DSL
+  
+  SELECTION_PERCENT = 0.25
+
+  class << self
+    attr_accessor :drag_item, :drop_type
+  end
+
+  def initialize()
+    super
+
+    set_on_drag_over do |event|
+      if !child_of_target? && !over_self?
+        if drop_into_range? y_percentage(event)
+          set_effect inner_shadow(offset_x: 1.0)
+          self.class.drop_type = :drop_into
+        else
+          set_effect nil
+          self.class.drop_type = :rearrange
+        end
+
+        event.accept_transfer_modes TransferMode::MOVE
+      end
+    end
+
+    set_on_drag_detected do |event|
+      drag_item = tree_item
+      content = clipboard_content { put_string drag_item.value }
+      tree_view.start_drag_and_drop(TransferMode::MOVE).set_content content
+      self.class.drag_item = drag_item
+      event.consume
+    end
+
+    set_on_drag_dropped do |event|
+      if drag_item && tree_item
+        drop_into if drop_type == :drop_into
+        rearrange(event) if drop_type == :rearrange
+
+        self.class.drag_item = nil
+        event.drop_completed = true
+      end
+
+      event.consume
+    end
+
+    set_on_drag_exited do |event|
+      set_effect nil
+    end
+  end
+
+  def y_percentage(event)
+    y = event.scene_y - local_to_scene(0, 0).y
+    y == 0 ? 0 : y / height
+  end
+
+  def child_of_target?(parent = tree_item)
+    return true if drag_item == parent
+    return false if !parent || !parent.parent
+    child_of_target?(parent.parent)
+  end
+
+  def drop_into
+    if !child_of_target? && !over_self?
+      drag_item.parent.children.remove(drag_item)
+      tree_item.children.add(drag_item)
+      tree_item.expanded = true
+    end
+  end
+
+  def drop_into_range?(percent)
+    percent >= SELECTION_PERCENT && percent <= (1-SELECTION_PERCENT)
+  end
+
+  def over_self?
+    drag_item.parent == tree_item
+  end
+
+  def updateItem(item, empty)
+    super(item, empty);
+    self.text = item if item
+  end
+
+  def drag_item
+    self.class.drag_item
+  end
+
+  def drop_type
+    self.class.drop_type
+  end
+
+  def rearrange(event)
+    parent = tree_item.parent
+
+    unless parent # root of tree view
+      parent = tree_item 
+      where = 0
+    end
+
+    drag_item.parent.children.remove(drag_item)
+    saved_items = parent.children.to_a
+
+    unless where # where already deduced from root being view_item 
+      where = saved_items.find_index { |e| e == tree_item } 
+      where += 1 if y_percentage(event) > SELECTION_PERCENT
+    end
+
+    if (where >= saved_items.size)
+      parent.children.add(drag_item)
+    else
+      parent.children.set(where, drag_item)
+      where.upto(saved_items.size - 2) do |i|
+        parent.children.set(i+1, saved_items[i])
+      end
+      parent.children.add(saved_items[saved_items.size - 1])
+    end
+  end
+end
 
 class SimpleTreeView < JRubyFX::Application
   def start(stage)
     with(stage, width: 300, height: 300, title: 'Simple Tree View') do
-      layout_scene(:dark_blue) do
-        stack_pane do
-          tree_view do |t|
-            tree_item("root", expanded: true) do |r|
-              3.times { |i| tree_item("Item #{i}") }
+      layout_scene(:blue) do
+        stack_pane(padding: insets(30)) do
+          tree_view(cell_factory: proc { DraggableTreeCell.new}) do
+            tree_item("Root") do
+              5.times {|i| tree_item "File #{i}" }
             end
           end
         end
