4796958d4bceead2474962743d9167fc30f7f2ea
Patrick Plenefisch <simonpatp@gmail.com>
2013-02-11 22:03:28 -0500

    Initial split of FXController

--- lib/jrubyfx/fxml_controller.rb
+++ lib/jrubyfx/fxml_controller.rb
@@ -16,22 +16,11 @@ limitations under the License.
 =end
 
 require 'jrubyfx'
+require_relative 'fxml_controller_base'
 
 # Inherit from this class for FXML controllers
 class JRubyFX::Controller
-  include JRubyFX
-  include JRubyFX::DSL
-  java_import 'java.net.URL'
-  java_import 'javafx.fxml.FXMLLoader'
-
-  @@default_settings = {
-    width: -1,
-    height: -1,
-    fill: :white,
-    depth_buffer: false,
-    relative_to: nil,
-    initialized: nil,
-  }
+  include JRubyFX::ControllerBase
 
   # Controllers usually need access to the stage.
   attr_accessor :stage, :scene
@@ -72,7 +61,7 @@ class JRubyFX::Controller
     ctrl.stage = stage
 
     # load the FXML file
-    root = get_fxml_loader(filename, ctrl, settings[:relative_to]).load
+    root = ControllerBase.get_fxml_loader(filename, ctrl, settings[:relative_to]).load
 
     # Unless the FXML root node is a scene, wrap that node in a scene
     if root.is_a? Scene
@@ -95,187 +84,4 @@ class JRubyFX::Controller
     ctrl
   end
 
-  # FXMLLoader#load also calls initialize
-  # if defined, move initialize so we can call it when we're ready
-  def self.method_added(meth)
-    if meth == :initialize and not @ignore_method_added
-      @ignore_method_added = true
-      alias_method :initialize_callback, :initialize
-      self.send(:define_method, :initialize) {|do_not_call_me|}
-    end
-  end
-
-
-  ##
-  # Event Handlers
-  ##
-
-  ##
-  # call-seq:
-  #   on(callback) { |event_info| block } => Method
-  #   on(callback, EventType) { |event_info| block } => Method
-  #   on_type(callback) { |event_info| block } => Method
-  #
-  # Registers a function of name `name` for a FXML defined event with the body in the block
-  # Note: there are overrides for most of the default types, so you should never
-  # need to manually specify the `type` argument unless you have custom events.
-  # The overrides are in the format on_* where * is the event type (ex: on_key for KeyEvent).
-  #
-  # === Convienence Methods
-  # * on_key           is for KeyEvent
-  # * on_mouse         is for MouseEvent
-  # * on_touch         is for TouchEvent
-  # * on_gesture       is for GestureEvent
-  # * on_context       is for ContextMenuEvent
-  # * on_context_menu  is for ContextMenuEvent
-  # * on_drag          is for DragEvent
-  # * on_ime           is for InputMethodEvent
-  # * on_input_method  is for InputMethodEvent
-  # * on_window        is for WindowEvent
-  # * on_action        is for ActionEvent
-  # * on_generic       is for Event
-  #
-  # === Examples
-  #   on :click do
-  #     puts "button clicked"
-  #   end
-  #
-  #   on_mouse :moved do |event|
-  #     puts "Mouse Moved"
-  #     p event
-  #   end
-  #
-  #   on_key :keypress do
-  #     puts "Key Pressed"
-  #   end
-  #
-  # === Equivalent Java
-  #   @FXML
-  #   private void click(ActionEvent event) {
-  #     System.out.println("button clicked");
-  #   }
-  #
-  #   @FXML
-  #   private void moved(MouseEvent event) {
-  #     System.out.println("Mouse Moved");
-  #   }
-  #
-  #   @FXML
-  #   private void keypress(KeyEvent event) {
-  #     System.out.println("Key Pressed");
-  #   }
-  #
-  def self.on(names, type=ActionEvent, &block)
-    [names].flatten.each do |name|
-      class_eval do
-        # must define this way so block executes in class scope, not static scope
-        define_method name, block
-        # the first arg is the return type, the rest are params
-        add_method_signature name, [Void::TYPE, type]
-      end
-    end
-  end
-
-  # Get the metaclass, the singleton class of self, and add event handlers as on_EVENT
-  # This syntax allows us to define methods in class scope (eg: def self.on_touch )
-  class << self
-    {
-      :key          => KeyEvent,
-      :mouse        => MouseEvent,
-      :touch        => TouchEvent,
-      :gesture      => GestureEvent,
-      :context      => ContextMenuEvent,
-      :context_menu => ContextMenuEvent,
-      :drag         => DragEvent,
-      :ime          => InputMethodEvent,
-      :input_method => InputMethodEvent,
-      :window       => WindowEvent,
-      :action       => ActionEvent,
-      :generic      => Event
-    }.each do |method, klass|
-      # define the handy overloads that just pass our arguments in
-      define_method("on_#{method}") { |name, &block| on name, klass, &block }
-    end
-  end
-
-
-  ##
-  #  Node Lookup Methods
-  ##
-
-  # searches for an element by id (or fx:id, prefering id)
-  def method_missing(meth, *args, &block)
-    # if scene is attached, and the method is an id of a node in scene
-    if @scene
-      @nodes_by_id[meth] ||= find "##{meth}"
-      return @nodes_by_id[meth] if @nodes_by_id[meth]
-    end
-
-    super
-  end
-
-  # return first matched node or nil
-  def find(css_selector)
-    @scene.lookup css_selector
-  end
-
-  # Return first matched node or throw exception
-  def find!(css_selector)
-    res = find(css_selector)
-    raise "Selector(#{css_selector}) returned no results!" unless res
-    res
-  end
-
-  # return an array of matched nodes
-  def css(css_selector)
-    @scene.get_root.lookup_all(css_selector).map {|e| e}
-  end
-
-
-  ##
-  # call-seq:
-  #   get_fxml_loader(filename) => FXMLLoader
-  #   get_fxml_loader(filename, controller_instance) => FXMLLoader
-  #   get_fxml_loader(filename, controller_instance, relative_to) => FXMLLoader
-  #
-  # Load a FXML file given a filename and a controller and return the loader
-  # relative_to can be a file that this should be relative to, or an index
-  # of the caller number.
-  # === Examples
-  #   root = JRubyFX::Controller.get_fxml_loader("Demo.fxml").load
-  #
-  #   root = JRubyFX::Controller.get_fxml_loader("Demo.fxml", my_controller).load
-  #
-  # === Equivalent Java
-  #   Parent root = FXMLLoader.load(getClass().getResource("Demo.fxml"));
-  #
-  def self.get_fxml_loader(filename, controller = nil, relative_to = nil)
-    fx = FXMLLoader.new
-    fx.location =
-      if JRubyFX::Application.in_jar?
-        # If we are in a jar file, use the class loader to get the file from the jar (like java)
-        JRuby.runtime.jruby_class_loader.get_resource filename
-      else
-        # caller[0] returns a string like so:
-        # "/home/user/.rvm/rubies/jruby-1.7.1/lib/ruby/1.9/irb/workspace.rb:80:in `eval'"
-        relative_to ||= caller[1][/(.*):[0-9]+:in /, 1] # the 1 is the first match, aka everything up to the :
-        # If we are in the normal filesystem, create a file url path relative to relative_to or this file
-        URL.new "file:#{File.join File.dirname(relative_to), filename}"
-      end
-    # we must set this here for JFX to call our events
-    fx.controller = controller
-    fx
-  end
-
-  ##
-  # call-seq:
-  #   load_fxml_control(filename, controller_root)
-  #
-  # Loads given filename as the markup for the controller (custom controls)
-  #
-  def self.load_fxml_control(filename, root)
-    fx = get_fxml_loader(filename, root)
-    fx.root = root
-    fx.load
-  end
 end
