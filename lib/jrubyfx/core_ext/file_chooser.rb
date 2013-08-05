d68be4b28f932c096cdb009ac3153a175e8081bf
Patrick Plenefisch <simonpatp@gmail.com>
2013-08-05 09:15:28 -0400

    file chooser formats

--- lib/jrubyfx/core_ext/file_chooser.rb
+++ lib/jrubyfx/core_ext/file_chooser.rb
@@ -24,9 +24,9 @@ class Java::javafx::stage::FileChooser
   #   add_extension_filter(description)
   #   add_extension_filter(description, filter)
   #   add_extension_filter(description, [filter, ...])
-  # 
+  #
   # Takes ether a straight descriptions with embedded (*.whatnot) filter, or
-  # separately, where filter can be an array or a string. Note that without a 
+  # separately, where filter can be an array or a string. Note that without a
   # filter, the description MUST contain a list of extensions in parens.
   # === Examples
   #   add_extension_filter("Ruby Files (*.rb)")
@@ -42,17 +42,17 @@ class Java::javafx::stage::FileChooser
     filter = [filter] unless filter.is_a? Array
     extension_filters.add(ExtensionFilter.new(desc.to_s, filter))
   end
-  
+
   # call-seq:
   #   add_extension_filters([description, ...])
   #   add_extension_filters({description => filter, ...})
   #   add_extension_filters({description => [filter, ...], ...})
-  # 
+  #
   # Takes a straight list of descriptions with embedded (*.whatnot) filters, or
   # a hash of "description" => "*.whatnot" or a hash of "description => ["*.whatnot", "*.etc"]
   # === Examples
   #   add_extension_filters(["Ruby Files (*.rb)", "Python Files (*.py)"])
-  #   add_extension_filters({"Ruby Files (*.rb)" => "*.rb", "Python Files (*.py)" => ["*.py", "*.pyc"]})
+  #   add_extension_filters("Ruby Files (*.rb)" => "*.rb", "Python Files (*.py)" => ["*.py", "*.pyc"])
   #
   def add_extension_filters(filters)
     #works with both arrays and hashes
